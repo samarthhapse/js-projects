8981fa7023bd8276c5bf6f80059238b5cf1d3b06
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2024-01-05 15:46:23 +0530

    Add files via upload

---
+++ Insta Like Feature/script.js
@@ -0,0 +1,12 @@
+var cont= document.querySelector("#container")
+var love= document.querySelector("i")
+cont.addEventListener("dblclick",function(){
+    love.style.transform = 'translate(-50%,-50%) scale(1)'
+    love.style.opacity = 1
+    setTimeout(function(){
+    love.style.opacity = 0
+    },1000)
+    setTimeout(function(){
+    love.style.transform = 'translate(-50%,-50%) scale(0)'
+    },3000)
+})
