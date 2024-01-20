864d77436a719da5edbe85a81a9c5b65ae7c5e11
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2024-01-20 23:04:32 +0530

    Add files via upload

---
+++ multi image hovering/script.js
@@ -0,0 +1,27 @@
+// var elem1=document.querySelector("#elem1")
+// var elemImage = document.querySelector("#elem1 img")
+
+// elem1.addEventListener("mousemove",function(dets){
+//     elemImage.style.left=dets.x+"px",
+//     elemImage.style.top=dets.y+"px"
+// })
+// elem1.addEventListener("mouseenter",function(dets){
+//     elemImage.style.opacity=1
+// })
+// elem1.addEventListener("mouseleave",function(dets){
+//     elemImage.style.opacity=0
+// })
+var elem = document.querySelectorAll(".elem")
+
+elem.forEach(function(val){
+    // console.log(val.childNodes)
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity=1;
+    })
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity=0;
+    })
+    val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left=dets.x+"px";
+    })
+})
